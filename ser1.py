8166139365107a7ef2a0356c463d7b4bcd76647a
cristysandu <crystisandu3@gmail.com>
2021-05-22 16:16:58 +0300

    Small changes to script for Rasberry pi

--- ser1.py
+++ ser1.py
@@ -4,19 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == '__main__':
-    print("aici")
     plt.close('all')
     plt.figure();
     plt.ion();
     plt.show();
-    print("aici")
     data = np.array([]);
     data1 = np.array([]);
     i = 0
     ser = serial.Serial('/dev/serial0', 9600, timeout=1)
     ser.flush()
     nextis =0
-    while i <= 200:
+    while i <= 100:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             if line == "H" :
@@ -28,10 +26,13 @@ if __name__ == '__main__':
                 nextis = 0
                 hm = float(line)
                 data1= np.append(data1, hm)
+            
             print(tmp)
+            print(hm)
+
             plt.cla()
-            plt.plot(data, label="T")
-            plt.plot(data1, label="H")
+            plt.plot(data, label="Hr")
+            plt.plot(data1, label="SO2")
             plt.pause(0.01)
             i= i+1
     plt.savefig('test.svg')
